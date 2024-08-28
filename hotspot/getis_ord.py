17655507cf3068bc8f8be30f8323ca3a20dc3923
Panagiotis Leftheriotis <p.leftheriotis@gmail.com>
2024-08-28 20:08:38 +0300

    Add files via upload

---
+++ hotspot/getis_ord.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+def calculate_getis_ord(self, file_GetisOrd):
+    while True:
+        try:
+            a = int(input("\nEnter the value of parameter a (a > 1): "))
+            if a <= 1:
+                print("The parameter 'a' must be greater than 1. Please enter a correct value.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a numeric value for parameter a.")
+
+    while True:
+        try:
+            k = int(input("\nEnter the distance for counting neighbours: "))
+            if k <= 0:
+                print("The distance for counting neighbours must be a positive integer. Please enter a correct value.")
+            elif k >= min(self.m, self.n, self.v):
+                print(f"The distance for counting neighbours is larger than the grid dimensions. Adjusting k to fit within grid boundaries.")
+                k = min(self.m, self.n, self.v) - 1  
+                break
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter an integer value for distance k.")
+    # Calculate mean and standard deviation
+    total_cells = self.m * self.n * self.v
+    counts_sum = 0
+    squared_counts_sum = 0
+
+    # Loop through all cells
+    for x in range(self.m):
+        for y in range(self.n):
+            for t in range(self.v):
+                cell = self.cells[x][y][t]
+                counts_sum += cell.count
+                squared_counts_sum += cell.count ** 2
+
+    # Calculate mean
+    mean = counts_sum / total_cells
+
+    # Calculate standard deviation
+    if (squared_counts_sum / total_cells) - (mean ** 2) > 0:
+        standard_deviation = np.sqrt((squared_counts_sum / total_cells) - (mean ** 2))
+    else:
+        standard_deviation = 1
+
+    #file_GetisOrd.write(f'Getis-Ord Statistic with a={a}\n')
+    #file_GetisOrd.write(f'Counting up to {k} neighbours\n')
+    #file_GetisOrd.write('\nCell Coordinates\tGetis-Ord Statistic\n')
+
+    # Loop for cell i
+    for x in range(self.m):
+        for y in range(self.n):
+            for t in range(self.v):
+                cell_i = self.cells[x][y][t]
+
+                # Initializing sums
+                weight_sum = 0 
+                squared_weight_sum = 0
+                weighted_count_sum = 0
+
+                # Loop for cell j
+                for b in range(max(0, x - k), min(self.m, x + k)):
+                    for c in range(max(0, y - k), min(self.n, y + k)):
+                        for d in range(max(0, t - k), min(self.v, t + k)):
+                            cell_j = self.cells[b][c][d]
+
+                            # Calculate the distance cell i from cell j
+                            distance_x = abs(x - b)
+                            distance_y = abs(y - c)
+                            distance_t = abs(t - d)
+
+                            # Calculate the maximum distance
+                            max_distance = max(distance_x, distance_y, distance_t)
+
+                            # Calculating weight i,j (w=a^1-r, a>1 , r=distance i,j)
+                            
+                            if max_distance <= k:    #Limiting the influence of further neighbours to gain computational efficiency
+                                weight = a ** (1 - max_distance)
+                            else:
+                                weight = 0 
+
+                            # Calculating sum of weights 
+                            weight_sum += weight
+
+                            # Calculating sum of squared weights 
+                            squared_weight_sum += weight ** 2
+
+                            # Add the weighted count to the sum for cell i
+                            weighted_count_sum += weight * cell_j.count
+
+                # Avoiding case were denominator = 0
+                denominator = (standard_deviation * np.sqrt(((total_cells * squared_weight_sum) - (weight_sum ** 2)) / (total_cells - 1)))
+                if denominator == 0:
+                    cell_i.getis_ord = 0
+                else:
+                    # Final calculation of Getis-Ord
+                    cell_i.getis_ord = (weighted_count_sum - (mean * weight_sum)) / denominator
+
+                print(f"[{x}][{y}][{t}] = {cell_i.getis_ord}")
+                file_GetisOrd.write(f"({x},{y},{t}) = {cell_i.getis_ord}\n")
+                
+                #if above threshold add to hotspots
+                threshold = 1.96
+                if cell_i.getis_ord > threshold:
+                    self.hotspots_getis_ord.append((x, y, t, cell_i.getis_ord))
+    
+    #prints to detect errors
+    #print(f"this is mean: {mean}")
+    #print(f"this is standard dev:{standard_deviation}")
+    
+    print("\nGetis-Ord calculated successfully")
---
+++ hotspot/percentile.py
@@ -0,0 +1,24 @@
+import numpy as np
+def calculate_90th_percentile(grid, file_90th):         
+    # Extract cell counts from cells
+    cell_counts = [cell.count for row in grid.cells for col in row for cell in col]
+    
+    # Calculate the 90th percentile
+    percentile_90 = np.percentile(cell_counts, 90)
+
+    # Write the 90th percentile in text file
+    #file_90th.write(f"90th-percentile: {percentile_90}\n")
+    print("\n90th-percentile calculated successfully")
+    
+    # Find hotspots based on the 90th percentile
+    flag = False
+    for x in range(grid.m):
+        for y in range(grid.n):
+            for t in range(grid.v):
+                cell = grid.cells[x][y][t]
+                if cell.count > percentile_90:
+                    grid.hotspots_90th.append((x, y, t, cell.count))
+                    file_90th.write(f'({x},{y},{t}) = {cell.count}\n')
+                    flag = True
+    if not flag:
+        print('Did not find any hotspots')  
